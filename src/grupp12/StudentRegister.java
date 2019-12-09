c05ead6ab7a6433803eeee1606d4203b3203f12b
davidcostaniuc <56583339+davidcostaniuc@users.noreply.github.com>
2019-12-09 16:15:25 +0100

    StudentRegister
    added get and set on ArayyList

--- src/grupp12/StudentRegister.java
+++ src/grupp12/StudentRegister.java
@@ -1,5 +1,16 @@
-package grupp12;
 
+package grupp12;
+import java.util.*;
 public class StudentRegister {
 
+	private ArrayList <Student> studentlist = new ArrayList <Student> ();
+
+	public ArrayList <Student> getStudentlist() {
+		return studentlist;
+	}
+
+	public void setStudentlist(ArrayList <Student> studentlist) {
+		this.studentlist = studentlist;
+	}
+	
 }
