8c30e2807acd994d6826411f989615e01d72719c
GabrielKohler <gabbe.kohler@gmail.com>
2019-12-09 16:12:01 +0100

    Created Course class

---
+++ src/grupp12/Course.java
@@ -0,0 +1,31 @@
+package grupp12;
+
+public class Course {
+	
+	private int courseCode;
+	private String name;
+	private int credit;
+	
+	public int getCourseCode() {
+		return courseCode;
+	}
+	public void setCourseCode(int courseCode) {
+		this.courseCode = courseCode;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getCredit() {
+		return credit;
+	}
+	public void setCredit(int credit) {
+		this.credit = credit;
+	}
+	
+	
+
+
+}
