2c9396bd519b10330c3550e80ce0f7122b866bc1
Hannacarling <56675180+Hannacarling@users.noreply.github.com>
2019-12-10 09:14:12 +0100

    Update Result
    Setters and getters

--- src/grupp12/Result.java
+++ src/grupp12/Result.java
@@ -1,27 +1,69 @@
 package grupp12;
 
+import java.util.ArrayList;
+
 public class Result {
-	
+
 	private int result;
 	private int letterGrade;
-	private Student s;
-	private WrittenExam WE;
-	
-	
-	public void  setResult (int result){
+	// private Student student;
+	// private WrittenExam writtenexam;
+	private ArrayList<WrittenExam> writtenexam = new ArrayList<WrittenExam>();
+	private ArrayList<Student> studentlist = new ArrayList<Student>();
+
+	public void setResult(int result) {
 		this.result = result;
-}
+	}
+
 	public int getResult() {
 		return result;
 	}
+
 	public void setLetterGrade(int letterGrade) {
 		this.letterGrade = letterGrade;
 	}
+
 	public int getLetterGrade() {
 		return letterGrade;
 	}
 
-	public void setS (Student s) {
-		this.result = s;
-		
+	// public void setS(Student student) {
+	// this.student = student;
+
+	// }
+
+//	public Student getStudent() {
+//		return student;
+//	}
+
+	// public void setWrittenExam(WrittenExam writtenexam) {
+	// this.writtenexam = writtenexam;
+	// }
+
+	// public WrittenExam getWrittenexam() {
+	// return writtenexam;
+	// }
+	public void setWrittenexam(ArrayList<WrittenExam> writtenexam) {
+		this.writtenexam = writtenexam;
 	}
+
+	public ArrayList<WrittenExam> getWrittenexam() {
+		return writtenexam;
+	}
+
+	public void setStudentlist(ArrayList<Student> studentlist) {
+		this.studentlist = studentlist;
+	}
+
+	public ArrayList<Student> getStudentlist() {
+		return studentlist;
+	}
+
+	public void addResult(Student result) {
+		studentlist.add(result);
+	}
+
+	public void addLetterGrade(WrittenExam letterGrade) {
+		writtenexam.add(letterGrade);
+	}
+}
