926a19534364ae3b0cfe01bb81dc1a6130c0e519
GabrielKohler <gabbe.kohler@gmail.com>
2019-12-10 08:03:16 +0100

    Upload Course class to master

---
+++ Course.java
@@ -0,0 +1,50 @@
+package grupp12;
+
+import java.util.ArrayList;
+
+public class Course {
+	
+	private String courseCode;
+	private String name;
+	private int credit;
+	private ArrayList <Writtenexam> writtenExamRegister = new ArrayList <Writtenexam>();
+	private CourseRegister courseRegister;
+	
+	public String getCourseCode() {
+		return courseCode;
+	}
+	public void setCourseCode(String courseCode) {
+		this.courseCode = courseCode;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getCredit() {
+		return credit;
+	}
+	public void setCredit(int credit) {
+		this.credit = credit;
+	}
+	
+	public ArrayList <Writtenexam> getWrittenExamRegister() {
+		return writtenExamRegister;
+	}
+	public void setWrittenExamRegister(ArrayList <Writtenexam> writtenExamRegister) {
+		this.writtenExamRegister = writtenExamRegister;
+	}
+	public void addWrittenExam(Writtenexam exam) {
+		writtenExamRegister.add(exam);
+	}
+	public void removeWrittenExam(Writtenexam exam) {
+		writtenExamRegister.remove(exam);
+	}
+	public CourseRegister getCourseRegister() {
+		return courseRegister;
+	}
+	public void setCourseRegister(CourseRegister courseRegister) {
+		this.courseRegister = courseRegister;
+	}
+}
