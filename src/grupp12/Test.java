d4b0b04687de330847adda05d3c26b282f8803d8
GabrielKohler <gabbe.kohler@gmail.com>
2019-12-11 08:18:18 +0100

    Created Test class

---
+++ src/grupp12/Test.java
@@ -0,0 +1,14 @@
+package grupp12;
+
+public class Test {
+	public static void main(String [] args) {
+		
+		Course c = new Course();
+		CourseRegister cRegister = new CourseRegister();
+		Result r = new Result();
+		Student s = new Student();
+		StudentRegister sRegister = new StudentRegister();
+		WrittenExam writtenE = new WrittenExam();
+	}
+
+}
