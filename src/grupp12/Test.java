d9117ef183385699e7fece07d2a694fd832d08c8
GabrielKohler <gabbe.kohler@gmail.com>
2019-12-11 08:25:08 +0100

    Updated Test class

--- src/grupp12/Test.java
+++ src/grupp12/Test.java
@@ -4,7 +4,19 @@ public class Test {
 	public static void main(String [] args) {
 		
 		Course c = new Course();
+		
+		c.setCourseCode("sysa21");
+		c.setName("ISProjekt");
+		c.setCredit(30);
+		
+		
 		CourseRegister cRegister = new CourseRegister();
+		
+		cRegister
+		
+		
+		
+		
 		Result r = new Result();
 		Student s = new Student();
 		StudentRegister sRegister = new StudentRegister();
