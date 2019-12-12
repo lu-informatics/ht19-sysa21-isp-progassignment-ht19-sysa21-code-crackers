7a2925824f2ddf0efbacb1ec24f3b75d04e6cacd
davidcostaniuc <56583339+davidcostaniuc@users.noreply.github.com>
2019-12-12 10:04:38 +0100

    Update CourseRegister.java

--- src/grupp12/CourseRegister.java
+++ src/grupp12/CourseRegister.java
@@ -4,31 +4,36 @@ import java.util.HashMap;
 
 public class CourseRegister {
 
-private HashMap<String, Course> courslist = new HashMap<String, Course>();
+private HashMap<String, Course> courselist = new HashMap<String, Course>();
 
-public HashMap<String, Course> getCourlist() {
-	return courslist;
+public HashMap<String, Course> getCourselist() {
+	return courselist;
 }
 
 public void setCourlist(HashMap<String, Course> courlist) {
-	this.courslist = courlist;
+	this.courselist = courlist;
 }
 
 public void add(Course course) {
-	this.courslist.put(Course.getCourseCode(), course);
+	this.courselist.put(Course.getCourseCode(), course);
 }
 
 public Course findCourse(String coursCode) {
-	return this.courslist.get(coursCode);
+	return this.courselist.get(coursCode);
 }
 
 public void remove(String courseCode) {
-	this.courslist.remove(courseCode);
-}
+	this.courselist.remove(courseCode);
+} 
 	
-public boolean replace(coursCode, name, newName)
-}
+public boolean replace(courseCode, oldName, newName) {
+	Course curName= Course.get(name);
+	if ( !Course.equals(curName, oldName) || (curName == null && !containsKey(courseCode))) {
+		return false;
+	}
 
+}
+}
 				
 
 
