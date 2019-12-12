ddb976801b8aa123e74bd81c646822dba8ca46b1
Hannacarling <56675180+Hannacarling@users.noreply.github.com>
2019-12-12 11:17:00 +0100

    Update Student.java
    First try to implement the counter

--- src/grupp12/Student.java
+++ src/grupp12/Student.java
@@ -1,37 +1,48 @@
 package grupp12;
+
 import java.util.*;
+
 public class Student {
 	private static String studentID;
-	private static  String name;
+	private static String name;
 	private StudentRegister studentRegister;
-	private HashMap <String, Result> studentResultList= new HashMap <String, Result> ();
-	
-	
+	private HashMap<String, Result> studentResultList = new HashMap<String, Result>();
+	// private BasicCounter counter = new BasicCounter();
+
 	public static String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public static String getStudentID() {
 		return studentID;
 	}
+
 	public void setStudentID(String studentID) {
 		Student.studentID = studentID;
-	} 
-
-	public void setStudentResultList(HashMap <String, Result> studentResultList) {
-		this.studentResultList= studentResultList;
 	}
-	
-	public HashMap <String, Result>getStudentResultList() {
+
+	public void setStudentResultList(HashMap<String, Result> studentResultList) {
+		this.studentResultList = studentResultList;
+	}
+
+	public HashMap<String, Result> getStudentResultList() {
 		return studentResultList;
-		
+
 	}
 
-	
 	public static boolean equals(String curName, String oldName) {
 		// TODO Auto-generated method stub
 		return false;
 	}
-} 
+
+	//int counterVarialbe = 10000;
+//	BasicCounter counter = new BasicCounter();
+
+	//public void up () {
+	//	++ this.countervariable
+
+}
