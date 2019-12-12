84cbe34c404cf0a9754d3d0283747a60b23a78e9
davidcostaniuc <56583339+davidcostaniuc@users.noreply.github.com>
2019-12-12 10:21:55 +0100

    Update CourseRegister.java

--- src/grupp12/CourseRegister.java
+++ src/grupp12/CourseRegister.java
@@ -29,7 +29,7 @@ public void remove(String courseCode) {
 public void replace(String courseCode, String oldName, String newName) {
 	String curName= Course.getName();
 	if ( !Course.equals(curName, oldName) || (curName == null && !containsKey(courseCode))) {
-		return;
+
 	}
 
 }
