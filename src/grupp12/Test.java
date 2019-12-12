21e092a12534732ad6ce7ea2ae044f02f64f5f0c
Hannacarling <56675180+Hannacarling@users.noreply.github.com>
2019-12-12 13:11:04 +0100

    Update Test.java
    Small fixes

--- src/grupp12/Test.java
+++ src/grupp12/Test.java
@@ -4,25 +4,25 @@ public class Test {
 	
 	public static void main(String [] args) {
 		
-		Course c = new Course();
+		Course course = new Course();
 		
-		c.setCourseCode("sysa21");
-		c.setName("ISProjekt");
-		c.setCredit(30);
+		course.setCourseCode("sysa21");
+		course.setName("ISProjekt");
+		course.setCredit(30);
 		
-		Student s = new Student();
+		Student student = new Student();
 		
-		s.setName("Gabriel");
-		s.setStudentID("8771"); // Ska ha 5 siffror och ett S i början! 
+		student.setName("Gabriel");
+		student.setStudentID(getStudentID()); // Ska ha 5 siffror och ett S i början! SKA HÄMTAS FRÅN EN COUNTER 
 		
 		CourseRegister cRegister = new CourseRegister();
 		
 		
 		
-		Result r = new Result();
+		Result result = new Result();
 		
-		r.setResult(87);
-		r.setLetterGrade("A");
+		result.setResult(87);
+		result.setLetterGrade("A");
 		// setLetterGrade String eller int, vad är bäst??
 		
 		
@@ -37,22 +37,22 @@ public class Test {
 		writtenE.setMaxpoints(100);
 		
 		//Kopplingar:
-		cRegister.addCourse(c);
-		c.setCourseRegister(cRegister);
-		writtenE.setCourse(c);
-		c.addWrittenExam(writtenE);
-		r.addStudent(s);
-		s.addResult(r);
+		cRegister.addCourse(course);
+		course.setCourseRegister(cRegister);
+		writtenE.setCourse(course);
+		course.addWrittenExam(writtenE);
+		result.addStudent(student);
+		student.addResult(result);
 		// lägg till en writtenExam till  en student
 		
 		// inte kopplat en student till ett resultat ev. "s.getExamresult();"
 		
-		s.setStudentRegister(sRegister);
-		sRegister.addStudent(s);
+		student.setStudentRegister(sRegister);
+		sRegister.addStudent(student);
 		
 		sRegister.updateStudentName("8771", "peter"); //Ska inte kunna uppdatera id, men kanske står för att det är en "key" för tt hitta rätt person? 
 		
-		System.out.println(s.getName());
+		System.out.println(student.getName());
 		
 		
 		
