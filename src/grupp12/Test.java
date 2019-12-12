d0c10eeebd3654dd4c3ecfec38f6d9074390111a
je5206bj-s <56588595+je5206bj-s@users.noreply.github.com>
2019-12-12 13:52:35 +0100

    Update test

--- src/grupp12/Test.java
+++ src/grupp12/Test.java
@@ -4,11 +4,57 @@ public class Test {
 	
 	public static void main(String [] args) {
 		
-		Course course = new Course();
+		Course c1 = new Course();
+		Course c2 = new Course();
 		
-		course.setCourseCode("sysa21");
-		course.setName("ISProjekt");
-		course.setCredit(30);
+		WrittenExam wE1 = new WrittenExam();
+		WrittenExam wE2 = new WrittenExam();
+		WrittenExam wE3 = new WrittenExam();
+		WrittenExam wE4 = new WrittenExam();
+		
+		Student s1 = new Student();
+		Student s2 = new Student();
+		Student s3 = new Student();
+		Student s4 = new Student();
+		
+		c1.setCourseCode("sysa21");
+		c1.setName("ISProjekt");
+		c1.setCredit(30);
+		
+		c2.setCourseCode("sysa22");
+		c2.setName("Modelering");
+		c2.setCredit(55);
+		
+	wE1.setExamID("E10000");
+	wE1.setDate("22 maj 2020");
+	wE1.setLocation("Spartavägen 1");
+	wE1.setTime("08:00");
+	wE1.setMaxpoints(100);
+	wE1.setCourse(c1);
+	
+	wE2.setExamID("E10001");
+	wE2.setDate("22 maj 2020");
+	wE2.setLocation("Spartavägen 1");
+	wE2.setTime("08:00");
+	wE2.setMaxpoints(100);
+	wE2.setCourse(c2);
+	
+	wE3.setExamID("E10002");
+	wE3.setDate("22 maj 2020");
+	wE3.setLocation("Spartavägen 1");
+	wE3.setTime("08:00");
+	wE3.setMaxpoints(100);
+	wE3.setCourse(c1);
+	
+	wE4.setExamID("E10003");
+	wE4.setDate("22 maj 2020");
+	wE4.setLocation("Spartavägen 1");
+	wE4.setTime("08:00");
+	wE4.setMaxpoints(100);
+	wE4.setCourse(c2);
+	
+	s1.getName()("S10000");
+	
 		
 		Student student = new Student();
 		
