b45c0277fc61125c838c080879c0b9093e00fe75
je5206bj-s <56588595+je5206bj-s@users.noreply.github.com>
2019-12-13 12:30:59 +0100

    update main

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,9 @@
 package application;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
@@ -9,7 +11,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.Load(this.getClass().getResource("/frame.fxml"));
+			Parent root = FXMLLoader.load(this.getClass().getResource("frame.fxml"));
 			Scene scene = new Scene(root, 400, 400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
