3171d1ca909316b3d10d3ecb89686ed68c99442e
Hannacarling <56675180+Hannacarling@users.noreply.github.com>
2019-12-13 08:26:31 +0100

    Update Test.java

--- src/grupp12/Test.java
+++ src/grupp12/Test.java
@@ -72,46 +72,11 @@ public class Test {
 		r1.setStudent(s1);
 		r1.setWrittenExam(wE3);
 
-		Student student = new Student();
+		
+		
 
-		student.setName("Gabriel");
-		student.setStudentID(getStudentID()); // Ska ha 5 siffror och ett S i början! SKA HÄMTAS FRÅN EN COUNTER
+	
 
-		CourseRegister cRegister = new CourseRegister();
-
-		Result result = new Result();
-
-		result.setResult(87);
-		result.setLetterGrade("A");
-		// setLetterGrade String eller int, vad är bäst??
-
-		StudentRegister sRegister = new StudentRegister();
-
-		WrittenExam writtenE = new WrittenExam();
-		writtenE.setExamID("JavaTenta1");
-		writtenE.setDate("12-12-2019");
-		writtenE.setLocation("Sparta1");
-		writtenE.setTime("08.00");
-		writtenE.setMaxpoints(100);
-
-		// Kopplingar:
-		cRegister.addCourse(course);
-		course.setCourseRegister(cRegister);
-		writtenE.setCourse(course);
-		course.addWrittenExam(writtenE);
-		result.addStudent(student);
-		student.addResult(result);
-		// lägg till en writtenExam till en student
-
-		// inte kopplat en student till ett resultat ev. "s.getExamresult();"
-
-		student.setStudentRegister(sRegister);
-		sRegister.addStudent(student);
-
-		sRegister.updateStudentName("8771", "peter"); // Ska inte kunna uppdatera id, men kanske står för att det är en
-														// "key" för tt hitta rätt person?
-
-		System.out.println(student.getName());
 
 	}
 
