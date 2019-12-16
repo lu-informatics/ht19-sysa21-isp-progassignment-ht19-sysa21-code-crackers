d670a42a0ffeb4dc06d34b1d89f31557636520cf
je5206bj-s <56588595+je5206bj-s@users.noreply.github.com>
2019-12-16 16:57:39 +0100

    dev

--- src/application/controller.java
+++ src/application/controller.java
@@ -10,10 +10,17 @@ import javafx.fxml.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+<<<<<<< Updated upstream
 import java.util.URL;
 
 
+=======
+
+
+
+>>>>>>> Stashed changes
 import java.util.ListResourceBundle;
+import java.util.Scanner;
 
 public class controller {
 	
@@ -29,7 +36,13 @@ public class controller {
 	@FXML
 	private TextField txtCourseCode;
 	@FXML
+<<<<<<< Updated upstream
 	private  txtCourseCredit;
+=======
+	private  TextField txtNewStudentName;
+	@FXML
+	private  TextField txtNewCourseName;
+>>>>>>> Stashed changes
 	@FXML
 	private Button btnAddStudent;
 	@FXML
@@ -66,17 +79,24 @@ public class controller {
 	public void btnAddCourse_Click(ActionEvent event) {
 		String courseName = txtCourseName.getText();
 		String courseCode = txtCourseCode.getText();
+<<<<<<< Updated upstream
 		//int courseCredit = txtCourseCredit.gette();
 		Course tmpCourse = new Course();
 		tmpCourse.setCourseCode(courseCode);
 		//tmpCourse.setCredit(courseCredit);
+=======
+												//int courseCredit = txtCourseCredit.gette();
+		Course tmpCourse = new Course();
+		tmpCourse.setCourseCode(courseCode);
+												//tmpCourse.setCredit(courseCredit);
+>>>>>>> Stashed changes
 		tmpCourse.setName(courseName);
 		courseRegister.addCourse(tmpCourse);
 
 	}
 	
 	public void btnRemoveCourse_Click(ActionEvent event) {
-		String courseName = txtCourseName.getText();
+															// String courseName = txtCourseName.getText();
 		String courseCode = txtCourseCode.getText();
 			Course tmpCourse = courseRegister.getCourseList().get(courseCode); 
 		
@@ -85,24 +105,37 @@ public class controller {
 			}
 					else 
 					tmpCourse.remove(courseCode);
+<<<<<<< Updated upstream
 				
+=======
+				txtInfoScreen.setText("The course has been removed from list.");
+>>>>>>> Stashed changes
 			}
 	
 	
 	public void btnRemoveStudent_Click(ActionEvent event) {
-		String studentName = txtStudentName.getText();
+																//String studentName = txtStudentName.getText();
 		String studentID = txtStudentID.getText();
+<<<<<<< Updated upstream
 		Student tmpStudent = studentRegister.removeStudent(studentID); // void, retrunera student 
+=======
+		Student tmpStudent = studentRegister.getStudentList().get(studentID); // void, retrunera student 
+>>>>>>> Stashed changes
 			if ( tmpStudent == null) {
-				txtInfoScreen.setText("The student you searched for could not be found.");
+					txtInfoScreen.setText("The student you searched for could not be found.");
+			
 			}
-		
+			else 
+				tmpStudent.getStudentRegister().removeStudent(studentID);
+					txtInfoScreen.setText("The student has been removed from list.");
+			
 
 	}
 	
-	public Student btnUpdateStudent_Click(ActionEvent event) {
+	public void btnUpdateStudent_Click(ActionEvent event) {
 		String studentName = txtStudentName.getText();
 		String studentID = txtStudentID.getText();
+<<<<<<< Updated upstream
 		String studentNewName;
 		
 		studentRegister.replace(studentID, studentNewName); // returnera student 
@@ -110,36 +143,55 @@ public class controller {
 			if ( tmpStudent == null) {
 				txtInfoScreen.setText("The student you searched for could not be found.");
 				
+=======
+		String studentNewName = txtNewStudentName.getText(); // skapade en ny textsträng.
+			studentRegister.replace(studentID, studentNewName); // hur retunerar vi tillbaka studenten??
+>>>>>>> Stashed changes
 				
+			txtInfoScreen.setText("the students name has been updated");
 			}
-			else 
-					txtInfoScreen.setText("The student you have choosen has been updated");
+				//if ( studentRegister.findStudent(studentID) == null) {
+				//	txtInfoScreen.setText("The student you searched for could not be found.");
+				
+			 
+				//else 
+					//studentRegister.replace(studentID, studentNewName); // returnera student	
+						//txtInfoScreen.setText("The student you have choosen has been updated");
+					
+						
+		
 
-	}
+	
 	
 	public void btnUpdateCourse_Click(ActionEvent event) {
 		String courseName = txtCourseName.getText();
 		String courseCode = txtCourseCode.getText();
-		String courseNewName;
-		Student tmpCourse = courseRegister.replace(courseCode, courseName);
-			if ( tmpCourse == null) {
-				txtInfoScreen.setText("The course you searched for could not be found.");
-			}
-			else 
-				txtInfoScreen.setText("The course you have choosen has been updated");
-		
-
+		String courseNewName = txtNewCourseName.getText(); // skapade en ny textsträng.
+		courseRegister.replace(courseCode, courseNewName); // hur retunerar vi tillbaka kursen? 
+			
+			txtInfoScreen.setText("The course name has been updated.");
+		//if ( courseRegister.getCourseList().get(courseCode) == null) {
+				//txtInfoScreen.setText("The course you searched for could not be found.");
+			
 	}
+		
+				//else 
+				//txtInfoScreen.setText("The course you have choosen has been updated");
+					
+					
+
+	
 	
 	//Vet inte om denna metod är rätt..
 	
 	public void btnFindStudent_Click(ActionEvent event) {
 		String studentID = txtStudentID.getText();
 		Student tmpStudent = studentRegister.findStudent(studentID);
-		
+			
+			txtInfoScreen.appendText(tmpStudent.getName() + ", " + tmpStudent.getStudentID());  
 		//txtInfoScreen.setText(tmpStudent.getStudentID());
 		//for (Student student : tmpStudent.getStudentRegister().findStudent(studentID)) { 
-			txtInfoScreen.appendText(tmpStudent.getName() + ", " + tmpStudent.getStudentID());  
+			
 		}
 
 	//}
@@ -149,13 +201,15 @@ public class controller {
 		public void btnFindCourse_Click(ActionEvent event) {
 			String courseCode = txtCourseCode.getText();
 			Course tmpCourse = courseRegister.findCourse(courseCode);
-			
+				txtInfoScreen.appendText(tmpCourse.getName() + ", " + tmpCourse.getCourseCode() + ", " + tmpCourse.getCredit());  
+
 			//txtInfoScreen.setText(tmpStudent.getStudentID());
 			//for (Student student : tmpStudent.getStudentRegister().findStudent(studentID)) { 
-				txtInfoScreen.appendText(tmpCourse.getName() + ", " + tmpCourse.getCourseCode() + ", " + tmpCourse.getCredit());  
 			}
-
 		//}
+		
+
+		
 	
 
 }
---
+++ src/grupp12/StudentRegister.java
@@ -0,0 +1,68 @@
+
+package grupp12;
+import java.lang.Math;
+import java.util.Random;
+import java.util.HashMap;
+
+public class StudentRegister {
+
+	private HashMap<String, Student> studentList = new HashMap<String, Student>();
+	
+
+	public HashMap<String, Student> getStudentList() {
+		return studentList;
+	}
+
+	public void setStudentList(HashMap<String, Student> studentList) {
+		this.studentList = studentList;
+	}
+  
+	public void addStudent(Student student) {
+		this.studentList.put(student.getStudentID(), student);
+	}
+
+	public Student findStudent(String studentID) {
+	  //	return this.studentList.get(studentID);
+		for (Student s : studentList.values()) {
+			if (s.getStudentID().equals(studentID)) {
+				
+				return s;
+				
+				
+	}
+	}
+				return null;
+	}
+
+	public void removeStudent(String studentID) {
+		this.studentList.remove(studentID);
+	}
+
+	public void replace(String studentID, String newName) {
+
+		Student tmp = findStudent(studentID);
+		if (tmp != null) {
+            tmp.setName(newName);
+			//String curName = tmp.getName();
+			//if (!Student.equals(curName, oldName) || (curName == null && !containsKey(studentID))) {
+				
+		}
+		public String generateID(Student studentID) {
+			int max = 99999; 
+		    int min = 10000; 
+		    int range = max - min + 10000; 
+
+		    for (int i = 10000; i < 99999; i++) { 
+		        int rand = (int)(Math.random() * range) + min;
+		        for (Student tmp : studentList.values()) { 
+		        if (i != null) {
+		        	String s ="S";
+		        	s += rand ;
+		        	return rand;
+		}
+}
+		}
+	}
+}
+
+
