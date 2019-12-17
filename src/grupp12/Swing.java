98a0b47d9be97c580755a71b0ae86dc12c671ad7
davidcostaniuc <56583339+davidcostaniuc@users.noreply.github.com>
2019-12-17 10:53:40 +0100

    update design in swing

--- src/grupp12/Swing.java
+++ src/grupp12/Swing.java
@@ -4,11 +4,13 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JTextField;
+import java.awt.Font;
+import javax.swing.JTabbedPane;
+import javax.swing.JPanel;
 
 public class Swing {
 
 	private JFrame frame;
-	private JTextField txtContosoUniversety;
 
 	/**
 	 * Launch the application.
@@ -42,10 +44,17 @@ public class Swing {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		txtContosoUniversety = new JTextField();
-		txtContosoUniversety.setText("Contoso Universety");
-		txtContosoUniversety.setBounds(49, 48, 203, 59);
-		frame.getContentPane().add(txtContosoUniversety);
-		txtContosoUniversety.setColumns(10);
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(0, 0, 821, 598);
+		frame.getContentPane().add(tabbedPane);
+		
+		JPanel panel = new JPanel();
+		tabbedPane.addTab("Student", null, panel, null);
+		
+		JPanel panel_1 = new JPanel();
+		tabbedPane.addTab("Course", null, panel_1, null);
+		
+		JPanel panel_2 = new JPanel();
+		tabbedPane.addTab("Register", null, panel_2, null);
 	}
 }
