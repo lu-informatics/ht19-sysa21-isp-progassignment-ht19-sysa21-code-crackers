fcf4d6d36844033043ad0814faeff128e2804316
je5206bj-s <56588595+je5206bj-s@users.noreply.github.com>
2020-01-13 15:13:41 +0100

    Update

--- src/control/ExamController.java
+++ src/control/ExamController.java
@@ -29,7 +29,7 @@ public class ExamController {
 			if (this.findExam(examID) == null) {
 				WrittenExam ex = new WrittenExam(date, location, time);
 				for (WrittenExam we : course.getWrittenExamList()) {
-					//if (ex.getTime() != we.getTime() && ex.getDate() != we.getDate()) {
+					if (ex.getTime() != we.getTime() && ex.getDate() != we.getDate()) {
 
 						ex.setExamID(examID);
 						ex.getMaxPoints();
@@ -37,12 +37,12 @@ public class ExamController {
 						ex.setLocation(location);
 						ex.setTime(time);
 						ex.setCourse(course);
-						this.examList.add(ex);
+						course.getWrittenExamList().add(ex);
 					
 						examAdded = true;
 					}
 				}
-			}
+			}}
 		}
 	//}
 
